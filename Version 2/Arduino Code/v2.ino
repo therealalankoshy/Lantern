c44a8eb4be3ec9b2b2d24be5b0493be951beb8f3
Abishek <abishekvelayudhan@gmail.com>
2018-12-16 10:39:32 +0530

    made the functionalities as different function

--- Version 2/Arduino Code/v2.ino
+++ Version 2/Arduino Code/v2.ino
@@ -1,5 +1,3 @@
-
-int Received_value [3] ;
 int Received_Bit_Check [3] ={0,0,0} ;
 float ReceptionTime = 5000; // 5 mins = 5000
 double EndTime;
@@ -12,7 +10,7 @@ uint32_t endTime_uS;
 int16_t values[128];
 int i,j;
 
-                          // 0 , 1 
+                                // 0 , 1 
 float Freq_TimeData[3][2] = { { 1100.0, 2100.0 },   // 1st bit 
                             { 3100.0, 4100.0 },   // 2nd bit
                             { 5100.0, 6100.0 } }; // 3rd bit
@@ -41,34 +39,6 @@ float Single_Point_DFT(int16_t inputArray[], int startingIndex, int endingIndex,
   return (RealSum * RealSum + ImaginarySum * ImaginarySum) / N;
 }
 
-int16_t Find_128_sample_values()
-{   
-    for(uint8_t n = 0; n<128; n++)
-    {
-       values[n] = analogRead(analogInPin);
-    }    
-    return(values);
-}
-
-float Find_DFT(float Test_Freq)
-{
-    startTime_uS = micros();
-    for(uint8_t n = 0; n<128; n++)
-    {
-       values[n] = analogRead(analogInPin);
-    }  
-    endTime_uS = micros();
-    float sampleFreq = 128.0*1000000.0/(endTime_uS - startTime_uS);
-    float dft = Single_Point_DFT(values, 0, 127, sampleFreq, Test_Freq);
-    return dft;
-}
-
-void setup() 
-{
-  Serial.begin(9600);
-  double EndTime = StartTime = 0;
-}
-
 int BinaryToDecimal(int binary[])
 {
   int i=0;
@@ -86,20 +56,41 @@ int BinaryToDecimal(int binary[])
   return decimal;
 }
 
-void loop() 
+float Find_DFT(float Test_Freq)
 {
+    startTime_uS = micros();
+    int values = Find_128_sample_values();
+    endTime_uS = micros();
+    float sampleFreq = 128.0*1000000.0/(endTime_uS - startTime_uS);
+    float dft = Single_Point_DFT(values, 0, 127, sampleFreq, Test_Freq);
+    return dft;
+}
+
+int Find_128_sample_values()
+{   
+    for(uint8_t n = 0; n<128; n++)
+    {
+       values[n] = analogRead(analogInPin);
+    }    
+    return(values);
+}
+
+
+int Received_value()
+{
+  int Received_value[3];
   StartTime = millis();
   while (Received_Bit_Check[0] == 0 || Received_Bit_Check[1] == 0 || Received_Bit_Check[2] == 0)
   {
     if ((EndTime - StartTime) < ReceptionTime)
     {
-      for ( i = 0 ; i<3 ; i++ )  
+      for (int i = 0 ; i<3 ; i++)  
       { 
         if (Received_Bit_Check[i] == 0)
         {
-          for ( j = 0 ; j< 2 ; j++ )
+          for (int j = 0 ; j< 2 ; j++)
           {
-            float DFT = Find_DFT(Freq_TimeData[i][j]);
+            int DFT = Find_DFT(Freq_TimeData[i][j]);
 
             if (DFT > Threshold_TimeData[i][j])
             {
@@ -124,4 +115,19 @@ void loop()
       break;
     }
   }
+  return Received_value;
+}
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  double EndTime = StartTime = 0;
+}
+
+
+void loop() 
+{
+    int Binary = Received_value();
+    int Decimal = BinaryToDecimal(Binary);
 }
